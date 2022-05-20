ad269cc6cf6558a2db68c75c41f2556035c789a4
Vincent Marsh <vincentmarsh01@gmail.com>
2022-05-20 22:29:09 +0300

    A banking system

---
+++ Account.java
@@ -0,0 +1,35 @@
+public class Account {
+    
+     //class variables
+ int balance;
+ int previousTransaction;
+ String CustomerName;
+ String CustomerID;
+
+ //Class constructor
+ Account (String cname, String cid) {
+     CustomerName= cname;
+     CustomerID= cid;
+ }
+ //Function for depositing money
+ void deposit(int amount){
+     if(amount!=0)
+     {
+         balance= balance + amount;
+         previousTransaction= amount;
+     }
+    }
+     //Function for withdrawing money
+ void withdraw(int amount){
+     if (amount!=0){
+        balance= balance-amount;
+        previousTransaction= -amount;
+     }
+     }
+    //function showing previous transaction
+    void getPreviousTransaction(){
+
+    }
+    public void showMenu() {
+    }
+}
---
+++ BANK.java
@@ -0,0 +1,7 @@
+public class BANK {
+public static void main (String[]args) {
+    Account tim= new Account( "Tim Startler", "A00111");
+    tim.showMenu();
+
+}
+}
